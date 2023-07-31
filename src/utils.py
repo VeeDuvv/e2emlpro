10c79910c829b9995c4ac15444df864a10563b38
[Vamsi Duvvuri] <vamsi.duvvuri@icloud.com>
2023-07-30 22:52:16 -0400

    a pickle file was created with transformed data

--- src/utils.py
+++ src/utils.py
@@ -13,11 +13,11 @@ from src.exception import CustomException
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-
         os.makedirs(dir_path, exist_ok=True)
-
         with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
+            # a pickle file is a binary file that contains the serialized version of the object. 
+            # a serialized object is a byte stream that contains the object's data and enough information to reconstruct the object in memory
 
     except Exception as e:
         raise CustomException(e, sys)
